6fc6f8ef645d084bea6bb7ec50447451349db6b2
skborey <skborey@gmail.com>
2020-01-14 23:00:41 +0700

    change response message base on doc

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 // add new task to list
 app.post('/notification', (req, res) => {
     console.log(req.body);
-    res.json(req.body)
+    res.json({"result": "SUCCESS", "msg": "OK"})
 })
 
 app.get('/', (req, res) => {
